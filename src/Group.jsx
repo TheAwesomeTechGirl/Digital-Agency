b49c70ca2eede10dfdf8bc98f984f4b4c5ea2dfe
TheAwesomeTechGirl <tukeglory98@gmail.com>
2024-02-20 12:10:53 +0100

    fixed error

--- src/Group.jsx
+++ src/Group.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Groupimg from '../src/images/Group.svg';
+import marked from '../src/images/marked.svg'
 
 function Group() {
   return (
@@ -21,22 +22,22 @@ function Group() {
            guidelines ever.</p>
           <ul className="flex flex-row md:flex-row text-blue-950">
             <li className="flex flex-row items-center font-medium text-sm mr-4 mb-2">
-              <img src='../src/images/marked.svg' alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
+              <img src={marked} alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
               <span>Medical and vision</span>
             </li>
             <li className="flex flex-row items-center font-medium text-sm mr-4 mb-2">
-              <img src='../src/images/marked.svg' alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
+              <img src={marked} alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
               <span>HSAs and FSAs</span>
             </li>
           </ul>
           <ul className="flex flex-row md:flex-row text-blue-950">
           <li className="flex flex-row items-center font-medium text-sm mb-2">
-              <img src='../src/images/marked.svg' alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
+              <img src={marked} alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
               <span>Life insurance</span>
             </li>
            
             <li className="flex flex-row  items-center font-medium text-sm mb-2 ml-12">
-              <img src='../src/images/marked.svg' alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
+              <img src={marked} alt='marked' className="h-2 w-3 mt-1 mr-1"></img>
               <span>Commuter benefits</span>
             </li>
           </ul>
