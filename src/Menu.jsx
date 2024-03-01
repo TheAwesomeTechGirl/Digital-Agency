0a845ec97bd10d254931f42a82d5e4b6f91f5f80
TheAwesomeTechGirl <tukeglory98@gmail.com>
2024-03-01 19:58:26 +0100

    finished all pages

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,6 +4,7 @@ import Logo from '../src/images/logo.svg';
 import dark from '../src/images/dark.svg';
 import close from '../src/images/Close.svg';
 
+
 function Menu() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
@@ -22,14 +23,16 @@ function Menu() {
         <li className="flex flex-row items-center text-gray-600 font-normal">
           <Link to='/' className="nav-links" onClick={closeMobileMenu}>Home</Link>
         </li>
-        <li className="flex flex-row items-center text-gray-600 font-normal">
-          <Link to='/'  className="nav-links" onClick={closeMobileMenu}>Advertise</Link>
-          <span className="ml-1">
-            <svg className="h-3 w-3 text-gray-800" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-            </svg>
-          </span>
-        </li>
+
+    <li className="flex flex-row items-center text-gray-600 font-normal">
+      <Link to='/' className="nav-links" onClick={closeMobileMenu}>Advertise</Link>
+      <span className="ml-1">
+        <svg className="h-3 w-3 text-gray-800" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+        </svg>
+      </span>
+    </li>
+
         <li className="flex flex-row items-center text-gray-600 font-normal">
           <Link to='/'  className="nav-links" onClick={closeMobileMenu}>Support</Link>
           <span className="ml-1">
